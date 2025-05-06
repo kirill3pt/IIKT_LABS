6ea69e8582bb7d0749d3967409db6145b3ccdf28
kirill3pt <kirill.dib9@yandex.ru>
2025-05-06 12:24:46 +0300

    Zadanie 1 + 2
    load Zadanie 1 + 2

---
+++ LAB4/Zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+
+namespace Zadanie1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int[,] array = new int[5, 7];
+            Random rand = new Random();
+
+            // заполняем массив случайными числами
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 7; j++)
+                    array[i, j] = rand.Next(50, 100);
+
+            // создаём таблицу для вывода
+            DataTable dt = new DataTable();
+            for (int j = 0; j < 7; j++)
+                dt.Columns.Add($"Столбец {j + 1}");
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow row = dt.NewRow();
+                for (int j = 0; j < 7; j++)
+                    row[j] = array[i, j];
+                dt.Rows.Add(row);
+            }
+
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+    }
+}
---
+++ LAB4/Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Zadanie2
+{
+    public partial class MainWindow : Window
+    {
+        private const double ExchangeRate = 0.012261; // взял курс на 29.04.2025: 1 рубль = 0,012261 доллара
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputRubles.Text == "Введите кол-во рублей")
+            {
+                outputDollars.Text = "Сначала введите сумму!";
+                return;
+            }
+
+            if (double.TryParse(inputRubles.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double rubles))
+            {
+                double dollars = rubles * ExchangeRate;
+                outputDollars.Text = $"Это примерно: {dollars:F2} $";
+            }
+            else
+            {
+                outputDollars.Text = "Ошибка: введите корректную сумму!";
+            }
+        }
+
+        private void InputRubles_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (inputRubles.Text == "Введите сумму в рублях")
+            {
+                inputRubles.Text = "";
+                inputRubles.Foreground = Brushes.Black;
+            }
+        }
+
+        private void InputRubles_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputRubles.Text))
+            {
+                inputRubles.Text = "Введите сумму в рублях";
+                inputRubles.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
