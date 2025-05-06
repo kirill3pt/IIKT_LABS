b9d48653883326f2f481bd8b72f668210bcc6273
kirill3pt <kirill.dib9@yandex.ru>
2025-05-06 12:25:39 +0300

    Zadanie 3 + 4
    load Zadanie 3 + 4

---
+++ LAB4/Zadanie3/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Zadanie3
+{
+    public partial class MainWindow : Window
+    {
+        private const double usd = 0.012261; //все курсы валют взяты на 29.04.2025 23:31 :) 
+        private const double eur = 0.010733;
+        private const double uah = 0.51;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputRubles.Text == "Введите сумму в рублях")
+            {
+                outputResult.Text = "Введите сумму!";
+                return;
+            }
+
+            if (double.TryParse(inputRubles.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double rubles))
+            {
+                double result = 0;
+                string currency = "";
+
+                if (radioUSD.IsChecked == true)
+                {
+                    result = rubles * usd;
+                    currency = "$.";
+                }
+                else if (radioEUR.IsChecked == true)
+                {
+                    result = rubles * eur;
+                    currency = "€.";
+                }
+                else if (radioUAH.IsChecked == true)
+                {
+                    result = rubles * uah;
+                    currency = "₴.";
+                }
+
+                outputResult.Text = $"Это примерно: {result:F2} {currency}";
+            }
+            else
+            {
+                outputResult.Text = "Ошибка: введите корректное число!";
+            }
+        }
+
+        private void InputRubles_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (inputRubles.Text == "Введите сумму в рублях")
+            {
+                inputRubles.Text = "";
+                inputRubles.Foreground = Brushes.Black;
+            }
+        }
+
+        private void InputRubles_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputRubles.Text))
+            {
+                inputRubles.Text = "Введите сумму в рублях";
+                inputRubles.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
---
+++ LAB4/Zadanie4/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Zadanie4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowTemperatures_Click(object sender, RoutedEventArgs e)
+        {
+            temperatureList.Items.Clear();
+
+            for (int celsius = 0; celsius <= 100; celsius += 5)
+            {
+                double fahrenheit = 1.8 * celsius + 32;
+                temperatureList.Items.Add($"{celsius} °C = {fahrenheit:F1} °F");
+            }
+        }
+    }
+}
